435948b0adacc58c1dc3ce2749a8e9ebaabdea99
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-11-22 14:19:16 +0000

    task3

--- utils/db.js
+++ utils/db.js
@@ -4,30 +4,17 @@ class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const dbName = process.env.DB_NAME || 'files_manager';
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-    this.client = new MongoClient(`mongodb://${host}:${port}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client.connect().then(() => {
-      this.db = this.client.db(dbName);
-      console.log('Connected to MongoDB');
-    }).catch((err) => console.error('MongoDB connection error:', err));
-  }
-
-  isAlive() {
-    return !!this.db;
-  }
-
-  async nbUsers() {
-    return this.db.collection('users').countDocuments();
-  }
-
-  async nbFiles() {
-    return this.db.collection('files').countDocuments();
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
   }
 }
 
-module.exports = new DBClient();
+const dbClient = new DBClient();
+module.exports = dbClient;
