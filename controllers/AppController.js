dce4365f766b9286fbaac0a76ccfd07a24350cc0
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-11-23 21:33:17 +0000

    new changes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,30 +1,32 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-  static async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
-
-    res.status(200).json({
-      redis: redisAlive,
-      db: dbAlive,
-    });
+  /**
+   * should return if Redis is alive and if the DB is alive too
+   * by using the 2 utils created previously:
+   * { "redis": true, "db": true } with a status code 200
+   */
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
   }
 
-  static async getStats(req, res) {
-    try {
-      const usersCount = await dbClient.nbUsers();
-      const filesCount = await dbClient.nbFiles();
-
-      res.status(200).json({
-        users: usersCount,
-        files: filesCount,
-      });
-    } catch (err) {
-      res.status(500).json({ error: 'Could not retrieve stats' });
-    }
+  /**
+   * should return the number of users and files in DB:
+   * { "users": 12, "files": 1231 }
+   *  with a status code 200
+   */
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
   }
 }
 
-module.exports = AppController;
+export default AppController;
